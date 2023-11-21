6c6ce2288aad89877cfae4e127e5dd6d4e1cf82f
Joao Marcelo Dapper <joaomdapper@gmail.com>
2023-11-21 20:13:31 -0300

    Loop Principal do game, sistema de: fechar, salvar, carregar e excluir adicionados

--- SpaceMarker.py
+++ SpaceMarker.py
@@ -21,3 +21,35 @@ icone_game = pygame.image.load("space.png")
 pygame.display.set_icon(icone_game)
 pygame.display.set_caption("Space Marker")
 font = pygame.font.Font(None,20)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_F10:
+                with open("pontos.json", "w") as f:
+                    json.dump(points, f)
+                print("Seus pontos foram salvos com sucesso!")
+            try:
+                if event.key == pygame.K_F11:
+                    with open("pontos.json", "r") as f:
+                        points = json.load(f)
+                    print("Seus pontos foram carregados com sucesso!")
+            except:messagebox.showinfo("Não Existe Pontos Salvos")
+            if event.key == pygame.K_F12:
+                points = []
+                print("Seus pontos foram excluídos com sucesso!")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                point = event.pos
+                name = open_dialog()
+                points.append((point, name))
+
+    render_points()
+    
+#Quit Pygame
+pygame.quit()
